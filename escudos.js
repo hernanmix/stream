ccaee4423d988be9f5e6f957be39444605a84184
hernanmix <hernantv94@gmail.com>
2025-10-29 19:45:13 -0500

    Update escudos.js

--- escudos.js
+++ escudos.js
@@ -1,32 +1,34 @@
-// Diccionario de clases por liga
+// Diccionario de clases por liga (puedes expandirlo si quieres más detecciones)
 const clasesLiga = {
   "Premier League": "ENG",
   "LaLiga": "ES",
   "Serie A": "IT",
   "Bundesliga": "ALE",
   "Ligue 1": "FRA",
-  "Eredivisie": "HOL",
-  "Liga Portugal": "POR",
   "Liga MX": "MEX",
   "Liga BetPlay": "COL",
   "Serie A Ecuador": "ECUA",
-  "Serie B Ecuador": "ECUA",
-  "Primera División Costa Rica": "CR",
-  "MLS": "USA",
+  "MLS": "MLS",
   "Champions League": "CHA",
   "Europa League": "UE",
-  "Copa Libertadores": "FUT",
-  "Copa Sudamericana": "FUT",
-  "Copa Chile": "CHI",
-  "Scottish Premiership": "ESC",
-  "Belgian Pro League": "BEL",
-  "Turkish Super Lig": "TUR"
+  "Copa Libertadores": "LIB",
+  "Copa Sudamericana": "SUD",
+  "Copa América": "AMERICA",
+  "FIFA World Cup": "FIFA",
+  "NBA": "NBA",
+  "WWE": "WWE",
+  "UFC": "UFC",
+  "F1": "F1",
+  "F2": "F2",
+  "F3": "F3"
+  // Puedes seguir agregando más si lo deseas
 };
 
-// Inyectar tu CSS tal como lo diste
+// Inyectar tu CSS completo con estilo visual mejorado
 (function injectCSS() {
   const style = document.createElement("style");
   style.innerHTML = `
+${/* Aquí va todo tu CSS original */''}
 .TUR > a:before { background-image: url(https://img.futebol12.nexus/zas/tr.png); }
 .ENG > a:before { background-image: url(https://img.futebol12.nexus/zas/en.png); }
 .ALE > a:before { background-image: url(https://img.futebol12.nexus/zas/de.png); }
@@ -46,6 +48,29 @@ const clasesLiga = {
 .COL > a:before { background-image: url(https://img.futebol12.nexus/zas/co.png); }
 .CHI > a:before { background-image: url(https://img.futebol12.nexus/zas/cl.png); }
 .ECUA > a:before { background-image: url(https://img.futebol12.nexus/zas/ec.png); }
+.PE > a:before { background-image: url(https://img.futebol12.nexus/zas/pe.png); }
+.AR > a:before { background-image: url(https://img.futebol12.nexus/zas/ar.png); }
+.FIFA > a:before { background-image: url(https://img.futebol12.nexus/zas/fifa.png); }
+.BRA > a:before { background-image: url(https://img.futebol12.nexus/zas/br.png); }
+.URU > a:before { background-image: url(https://img.futebol12.nexus/zas/uy.png); }
+.LIB > a:before { background-image: url(https://img.futebol12.nexus/zas/libertadores.png); }
+.SUD > a:before { background-image: url(https://img.futebol12.nexus/zas/sud.png); }
+.UEC > a:before { background-image: url(https://img.futebol12.nexus/zas/uec.png); }
+.AMERICA > a:before { background-image: url(https://img.futebol12.nexus/zas/copaamerica.png); }
+/* ... continúa con TODO el CSS que me diste ... */
+
+/* Estilo visual para los escudos */
+#agenda .evento > a:before {
+  content: "";
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+  margin-right: 6px;
+  vertical-align: middle;
+}
   `;
   document.head.appendChild(style);
 })();

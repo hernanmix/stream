bf7cc522b0b335d007d8568479c52d5b92093761
hernanmix <hernantv94@gmail.com>
2025-10-29 20:05:30 -0500

    Update ligas.js

--- ligas.js
+++ ligas.js
@@ -1,17 +1,13 @@
 
 // Diccionario de clases por liga
 const clasesLiga = {
-  "MLS": "MLS",
-  "Liga MX": "MEX",
+  "Brasileirão": "BRA",
   "Liga BetPlay": "COL",
   "Serie A Ecuador": "ECUA",
   "Serie B Ecuador": "ECUA",
   "Primera División Costa Rica": "CR",
-  "Brasileirão": "BRA",
-  "Copa Uruguay": "URU",
-  "Copa Argentina": "AR",
-  "CONCACAF Champions Cup": "CONCACAFCHA",
   "CONCACAF Central American Cup": "CENTRALAMERICANCUP",
+  "CONCACAF Champions Cup": "CONCACAFCHA",
   "Copa Libertadores": "LIB",
   "Copa Sudamericana": "SUD",
   "Premier League": "ENG",
@@ -21,18 +17,59 @@ const clasesLiga = {
   "Ligue 1": "FRA",
   "Champions League": "CHA",
   "Europa League": "UE",
-  "Conference League": "UEC",
-  "Copa del Rey": "ESCOPAREY",
-  "FA Cup": "ENGFACUP",
-  "DFB Pokal": "ALE1",
-  "FIFA World Cup": "FIFA",
-  "UEFA Euro": "EURO",
-  "Copa América": "AMERICA",
   "NBA": "NBA",
-  "EuroLeague": "EUROLEAGUE",
-  "Liga Nacional de Básquet": "ENDESA"
-  // Puedes seguir agregando más si lo deseas
+  "FIFA World Cup": "FIFA",
+  "Copa América": "AMERICA",
+  "MLS": "MLS"
+  // Puedes seguir ampliando este diccionario si lo deseas
 };
+
+// Inyectar todo tu CSS con escudos
+(function injectCSS() {
+  const style = document.createElement("style");
+  style.innerHTML = `
+/* Escudos por clase (fragmento, tú me diste todo esto) */
+.TUR > a:before { background-image: url(https://img.futebol12.nexus/zas/tr.png); }
+.ENG > a:before { background-image: url(https://img.futebol12.nexus/zas/en.png); }
+.ALE > a:before { background-image: url(https://img.futebol12.nexus/zas/de.png); }
+.FRA > a:before { background-image: url(https://img.futebol12.nexus/zas/fr.png); }
+.HOL > a:before { background-image: url(https://img.futebol12.nexus/zas/nl.png); }
+.POR > a:before { background-image: url(https://img.futebol12.nexus/zas/pt.png); }
+.MEX > a:before { background-image: url(https://img.futebol12.nexus/zas/mx.png); }
+.ES > a:before { background-image: url(https://img.futebol12.nexus/zas/es.png); }
+.IT > a:before { background-image: url(https://img.futebol12.nexus/zas/it.png); }
+.BEL > a:before { background-image: url(https://img.futebol12.nexus/zas/be.png); }
+.CR > a:before { background-image: url(https://img.futebol12.nexus/zas/cr.png); }
+.USA > a:before { background-image: url(https://img.futebol12.nexus/zas/us.png); }
+.NBA > a:before { background-image: url(https://img.futebol12.nexus/zas/nbanew2.png); }
+.BRA > a:before { background-image: url(https://img.futebol12.nexus/zas/br.png); }
+.COL > a:before { background-image: url(https://img.futebol12.nexus/zas/co.png); }
+.ECUA > a:before { background-image: url(https://img.futebol12.nexus/zas/ec.png); }
+.CHA > a:before { background-image: url(https://img.futebol12.nexus/zas/champions.png); }
+.UE > a:before { background-image: url(https://img.futebol12.nexus/zas/ue.png); }
+.LIB > a:before { background-image: url(https://img.futebol12.nexus/zas/libertadores.png); }
+.SUD > a:before { background-image: url(https://img.futebol12.nexus/zas/sud.png); }
+.FIFA > a:before { background-image: url(https://img.futebol12.nexus/zas/fifa.png); }
+.AMERICA > a:before { background-image: url(https://img.futebol12.nexus/zas/copaamerica.png); }
+.CENTRALAMERICANCUP > a:before { background-image: url(https://img.futebol12.nexus/zas/centralamericancup.png?new); }
+.CONCACAFCHA > a:before { background-image: url(https://img.futebol12.nexus/zas/concacafcham.png); }
+.MLS > a:before { background-image: url(https://img.futebol12.nexus/zas/mls.png); }
+
+/* Estilo visual para escudos */
+.estado::after {
+  content: "";
+  display: inline-block;
+  width: 22px;
+  height: 22px;
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+  margin-left: 6px;
+  vertical-align: middle;
+}
+  `;
+  document.head.appendChild(style);
+})();
 
 // Detectar clase por nombre
 function detectarClaseLiga(nombre) {
@@ -44,17 +81,19 @@ function detectarClaseLiga(nombre) {
   return null;
 }
 
-// Insertar clase en el evento
+// Insertar clase en el evento y aplicar escudo
 function insertarClasesLiga() {
   document.querySelectorAll('#agenda .evento').forEach(evento => {
     const nombreSpan = evento.querySelector('.nombre');
-    if (!nombreSpan) return;
+    const estadoSpan = evento.querySelector('.estado');
+    if (!nombreSpan || !estadoSpan) return;
 
     const nombre = nombreSpan.textContent.trim();
     const clase = detectarClaseLiga(nombre);
 
     if (clase && !evento.classList.contains(clase)) {
       evento.classList.add(clase);
+      estadoSpan.classList.add('estado'); // Asegura que tenga la clase para el ::after
     }
   });
 }

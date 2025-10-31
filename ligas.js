6abc9ec7949bf08e0e09dc9063cebd81829479bb
hernanmix <hernantv94@gmail.com>
2025-10-31 00:51:00 -0500

    Update ligas.js

--- ligas.js
+++ ligas.js
@@ -42,8 +42,8 @@ const escudos = {
   "Copa Chile": "https://img.futebol12.nexus/zas/cl.png",
   "Copa Paraguay": "https://img.futebol12.nexus/zas/paraguay.png",
   "Copa Venezuela": "https://img.futebol12.nexus/zas/ven.png",
-  "Copa Honduras": "https://img.futebol12.nexus/zas/honduras.png"
-  "Super Lig": "https://img.futebol12.nexus/zas/tr.png"
+  "Copa Honduras": "https://img.futebol12.nexus/zas/honduras.png",
+  "Super Lig": "https://img.futebol12.nexus/zas/tr.png",
   // Puedes seguir agregando más si lo deseas
 };
 

8732572832c9f8bfa57d8e0a955fa21ea601ff85
hernanmix <hernantv94@gmail.com>
2025-11-02 11:01:11 -0500

    Update script8.js

--- script8.js
+++ script8.js
@@ -3,17 +3,14 @@ function abrirReproductor(canal) {
   let url = "";
 
   if (canalLower.startsWith("http")) {
-    // URL completa → úsala tal cual
     url = canalLower;
-  } else if (canalesStream.includes(canalLower)) {
-    // Canal tipo stream
-    url = "https://hernanmix.github.io/stream/reproductor.html?stream=" + canalLower;
   } else {
-    // Canal tipo channel
-    url = "https://hsports4hd.blogspot.com/2025/09/reproductor-evento.html?channel=" + canalLower;
+    // Todos los canales tipo stream se abren en tu reproductor GitHub
+    url = "https://hernanmix.github.io/stream/reproductor.html?stream=" + canalLower;
   }
 
   const iframe = document.getElementById("reproductorIframe");
   iframe.src = url;
   document.getElementById("reproductorOverlay").style.display = "block";
 }
+

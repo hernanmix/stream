823b9e029f4848f038b0161d964659d64c2d1ebf
hernanmix <hernantv94@gmail.com>
2025-11-02 11:09:03 -0500

    Create script8.js

---
+++ script8.js
@@ -0,0 +1,15 @@
+function abrirReproductor(canal) {
+  const canalLower = canal.toLowerCase();
+  let url = "";
+
+  if (canalLower.startsWith("http")) {
+    url = canalLower;
+  } else {
+    // Todos los canales se abren en tu reproductor GitHub
+    url = "https://hernanmix.github.io/stream/reproductor.html?stream=" + canalLower;
+  }
+
+  const iframe = document.getElementById("iframeReproductor");
+  iframe.src = url;
+  document.getElementById("modalReproductor").style.display = "block";
+}

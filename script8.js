e2a4a6ce31768bcfae36f342648833aee0eb04ac
hernanmix <hernantv94@gmail.com>
2025-11-02 09:20:25 -0500

    Update script8.js

--- script8.js
+++ script8.js
@@ -58,7 +58,7 @@ const canalesStream = [
   "espn", "espn2", "espn3", "ecdf", "disney1", "disney2", "disney3", "disney19"
   "foxsports", "foxsports2", "foxsports3", "nba", "tnt", "tyc", "directv",
   "win", "goltv", "tves", "caracol", "rcn", "teleamazonas", "telefe", "winplusonline1", "fox_deportes_usa", "laligahypermotion", 
-  "usa_network", 
+  "usa_network", "espn_nl3", "espn_nl1"
 ];
 
 function abrirReproductor(canal) {

66b2b93278c193e81adda2dc0e84065aaeb9966f
hernanmix <hernantv94@gmail.com>
2025-11-02 09:14:36 -0500

    Update script8.js

--- script8.js
+++ script8.js
@@ -55,9 +55,9 @@ function formatoHora12(horaTexto) {
 
 // Detectar y abrir canal en reproductor flotante
 const canalesStream = [
-  "espn", "espn2", "espn3", "ecdf", "disney", "disney1", "disney2",
+  "espn", "espn2", "espn3", "ecdf", "disney1", "disney2", "disney3", "disney19"
   "foxsports", "foxsports2", "foxsports3", "nba", "tnt", "tyc", "directv",
-  "win", "goltv", "tves", "caracol", "rcn", "teleamazonas", "telefe", "winplusonline1", "FOX_DEPORTEW_USA"
+  "win", "goltv", "tves", "caracol", "rcn", "teleamazonas", "telefe", "winplusonline1", "fox_deportes_usa"
 ];
 
 function abrirReproductor(canal) {

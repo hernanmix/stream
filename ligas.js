828f12842993c2c9a79a2f17632378ee2c117425
hernanmix <hernantv94@gmail.com>
2025-11-05 19:24:35 -0500

    Update ligas.js

--- ligas.js
+++ ligas.js
@@ -14,7 +14,7 @@ const escudos = {
   "LaLiga": "https://img.futebol12.nexus/zas/es.png",
   "Liga Profesional Saudí": "https://bow.futebol12.nexus/zas/sa.png",
   "Serie A": "https://bow.futebol12.nexus/zas/it.png",
-  "LaLiga 2": "https://img.futebol12.nexus/zas/es.png",
+  "LaLiga 2": "https://bow.futebol12.nexus/zas/es.png",
   "Bundesliga": "https://bow.futebol12.nexus/zas/de.png",
   "Superliga Argentina": "https://bow.futebol12.nexus/zas/ar.png",
   "Ligue 1": "https://bow.futebol12.nexus/zas/fr.png",
